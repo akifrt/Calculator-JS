542e6c3ed959a08e11b9ace0ae3cd774dbdc7dd6
akifrt <91149807+akifrt@users.noreply.github.com>
2021-12-07 16:03:57 +0400

    JS

---
+++ Calculator JS/assets/JS/index.js
@@ -0,0 +1,28 @@
+let number1 = +prompt('input first number')
+let number2 = +prompt('input second number')
+
+
+function Sum(number1, number2){
+	return number1 + number2;
+}
+
+console.log(Sum(number1, number2))
+
+function Difference(number1, number2){
+	return number1 - number2;
+}
+
+console.log(Difference(number1, number2))
+
+function Multiplication(number1, number2){
+	return number1 * number2;
+}
+
+console.log(Multiplication(number1, number2))
+
+function Division(number1, number2){
+	return number1 / number2;
+}
+
+console.log(Division(number1, number2))
+
